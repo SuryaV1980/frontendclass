cc38e5c85f5fd57b1595756f090bca052c2c2436
Surya V <subsu341112@gmail.com>
2026-09-08 20:18:19 +0530

    updated on 09/08/2026

--- JS/Day-16/Exercise/assets/JS/index.js
+++ JS/Day-16/Exercise/assets/JS/index.js
@@ -1,8 +1,9 @@
 const submit = document.getElementById("submit")
 
 
-submit.addEventListener("click",()=>{
-
+submit.addEventListener("click",(element)=>{
+  // this is very important for security purpose(preventDefault())
+  element.preventDefault()
 
 const userName = document.getElementById("userName")
 const userPassword = document.getElementById("userPassword")
@@ -10,5 +11,17 @@ const userrePassword = document.getElementById("userrePassword")
 
 const datas = { userName, userPassword, userrePassword}
 
+const result = JSON.parse(localStorage.getItem("registerdata")) || []
+result.push(datas)
+
+console.log(result);
+
+localStorage.setItem("registerdata",JSON.stringify(result))
+
+alert("Welcome to The Gang")
+
+
+
+window.location.reload()
 
 })

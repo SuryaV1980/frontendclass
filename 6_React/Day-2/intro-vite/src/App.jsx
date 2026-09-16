935d9e34c8b21c23c1b381dad2b74475ef8aac4f
Surya V <subsu341112@gmail.com>
2026-09-16 17:43:14 +0530

    updated

---
+++ 6_React/Day-2/intro-vite/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+const App = (()=>{
+  return (<>
+  
+<h1>Hello universe</h1>    
+  
+  </>)
+})
+
+export default App
